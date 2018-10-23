0ac8b4157bee6596431b845c224a48fcb6b9c41c
Craig Bomba <talk2cbo@gmail.com>
2018-10-23 11:36:46 -0500

    more

--- server/public/assets/javascript/app.js
+++ server/public/assets/javascript/app.js
@@ -29,7 +29,7 @@ var video = document.querySelector('#camera-stream'),
 
   var constraints = {
       audio: false,
-      video: {faceingMode: 'environment'}
+      video: {faceingMode: {exact: 'environment'}}
   }
 
   // Request the camera.
