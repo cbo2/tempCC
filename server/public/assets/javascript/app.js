c32a0ab5f9de7504fe7774ccffc382adccbcfde2
Craig Bomba <talk2cbo@gmail.com>
2018-10-23 11:10:50 -0500

    more

--- server/public/assets/javascript/app.js
+++ server/public/assets/javascript/app.js
@@ -42,7 +42,7 @@ var video = document.querySelector('#camera-stream'),
 
       // Create an object URL for the video stream and
       // set it as src of our HTLM video element.
-      video.src = window.URL.createObjectURL(stream);
+      video.srcObject = window.URL.createObjectURL(stream);
 
       // Play the video element to start the stream.
       video.play();
